e2c233006f08918fedbc11fba562f0438becd324
Parvaggarawal01 <parvaggarwal130@gmail.com>
2026-02-11 09:33:12 +0530

    Adding Role based authorization

--- routes/artifacts.route.js
+++ routes/artifacts.route.js
@@ -5,11 +5,18 @@ import {
 } from "../controllers/artifact.controller.js";
 import { toggleLike, getLikes } from "../controllers/likes.controller.js";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
+import { authorizeRoles } from "../middleware/role.middleware.js";
 
 const router = express.Router();
 
 // Apply auth middleware to protect these routes
-router.post("/", authMiddleware, createArtifact);
+// Only ADMIN and EDITOR can create artifacts
+router.post(
+  "/",
+  authMiddleware,
+  authorizeRoles("ADMIN", "EDITOR"),
+  createArtifact,
+);
 router.get("/", authMiddleware, getAllArtifacts);
 
 // Like routes
